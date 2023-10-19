423cd4ef9696c04b87fe5c02063b124122e61588
wei <miguelxu124@.gmail.com>
2023-10-19 21:45:57 +0200

    commit

--- src/MenuItem.jsx
+++ src/MenuItem.jsx
@@ -7,20 +7,21 @@ const variants = {
     y: 0,
     opacity: 1,
     transition: {
-    //   y: { stiffness: 1000, velocity: -100 }
+      y: { stiffness: 1000, velocity: -100 }//animación de muelle, velocity es velocidad inical, stinffness es potnecia de muelle.
     }
   },
   closed: {
     y: 50,
     opacity: 0,
     transition: {
-    //   y: { stiffness: 1000 }
+      y: { stiffness: 1000 }
     }
   }
 };
 
 const colors = ["#FF008C", "#D309E1", "#9C1AFF", "#7700FF", "#4400FF"];
-
+const equipos=["Bilibili Gaming", "Cloud9", "G2 Esports", "T1", "MAD Lions"];
+const imgUrl=["../src/img/lol.jpg"]
 export const MenuItem = ({ i }) => {
   const style = { border: `2px solid ${colors[i]}` };
   return (
@@ -29,8 +30,9 @@ export const MenuItem = ({ i }) => {
       whileHover={{ scale: 1.1 }}
       whileTap={{ scale: 0.95 }}
     >
-      <img src="../src/img/lol.jpg" className="icon-placeholder" style={style}/>
-      <div className="text-placeholder" style={style}/>
+      <img src={"../src/img/lol.jpg"} className="icon-placeholder" style={style}/>
+      <div className="text-placeholder" style={style}>{equipos[i]}</div>
+      {/* la manera dinámica de insertar textos */}
     </motion.li>
   );
 };
