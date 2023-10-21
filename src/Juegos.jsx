40d65436d90db113d1b3bff72d588001198d8af4
wei <miguelxu124@.gmail.com>
2023-10-21 03:05:57 +0200

    ok

--- src/Juegos.jsx
+++ src/Juegos.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import { motion } from 'framer-motion'
-
+import { useState } from 'react';
 export const Juegos = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       <motion.div className='juegos'>
-        
+        <motion.div
+        layout
+        // data-isOpen={isOpen}
+        initial={{ borderRadius: 50 }}
+        className="parent"
+        onClick={() => setIsOpen(!isOpen)}
+       >
+        <motion.div layout className="child" />
+      </motion.div>
       </motion.div>
     </>
     
