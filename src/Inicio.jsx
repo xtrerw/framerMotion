7b5f4ece569656b0bd97ed18df0f1632adfaeb30
wei <miguelxu124@.gmail.com>
2023-10-21 02:53:23 +0200

    exhibición de las imagenes

--- src/Inicio.jsx
+++ src/Inicio.jsx
@@ -1,18 +1,31 @@
-import { motion,AnimatePresence } from 'framer-motion'
-import React from 'react'
+import { motion,AnimatePresence, wrap} from 'framer-motion'
+import React, { useState } from 'react'
+import bg1 from './img/bg1.jpg'
 import bg2 from './img/bg2.jpg'
+import bg3 from './img/bg3.jpg'
+import { useEffect } from 'react';
 
-
+const imgs=[bg1,bg2,bg3];
 export const Inicio = () => {
+  const [[pagina,direccion],setImg]=useState([0,0]);
+  const index = wrap(0,imgs.length,pagina);
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setImg([pagina + 1, 1]);
+    }, 3000);
+    return () => clearInterval(timer);
+  }, [pagina]);
   return (
     <motion.div className='inicio'>
-      <AnimatePresence initial={false}>
+      <AnimatePresence initial={false} custom={direccion}>
         <motion.img
-          src={bg2}
-          initial={{ opacity: 0 }} // 初始透明度为0
-          animate={{ opacity: 1 }} // 动画结束时透明度为1
-          exit={{ opacity: 0 }}// 退出时透明度为0
-          transition={{ delay: 3,duration: 3}} 
+        className='imagen'
+          key={pagina}
+          src={imgs[index]}
+          custom={direccion}
+          // initial={{opacity:0}}
+          // animate={{opacity:1}}
+          // exit={{opacity:0}}
         />
       </AnimatePresence>
     </motion.div>
